0312383b2b2903e1a5cbf80594c87d1209a8b955
Gagan41 <gagant.p100@gmail.com>
2025-10-09 09:21:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Hello from "./helloworld";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis vero
+        beatae corporis modi facilis omnis quibusdam distinctio a accusamus
+        nulla perspiciatis ullam sint, iure illum eos consequatur hic
+        dignissimos optio?
+      </p>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
