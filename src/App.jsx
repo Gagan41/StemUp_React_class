b393e09cf9c59afa19ab668f34596848f0e27aea
Gagan41 <gagant.p100@gmail.com>
2025-10-10 11:51:51 +0530

    10-10-25

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import "./App.css";
 //import Hello from "./helloworld";
-import FirstComponent from "./firstComponent";
-import Fruit from "./classOne";
-import Parent from "./propsChildren";
+// import FirstComponent from "./firstComponent";
+// import Fruit from "./classOne";
+// import Parent from "./propsChildren";
+import ClassResult from "./conditionalRendering";
+import LogicalAND from "./logicalAND";
+import Destructuring1 from "./destructuring1";
+import Destructuring2 from "./destructuring2";
+import Parent from "./propDrilling";
 
 function App() {
   return (
     <div className="App">
-      <FirstComponent name="Gagan" />
+      {/* <FirstComponent name="Gagan" /> */}
       {/* <FirstComponent name="Singh" />
       <FirstComponent name="Yadav" /> */}
-      <Fruit />
-      <Parent />
+      {/* <Fruit />
+      <Parent /> */}
+      <ClassResult isresult={false}/>
+      <LogicalAND brand="Ford"/>
+      <Destructuring1 color="Red"/>
+      <Destructuring2 brand="Ford" model="Mustang" />
+      <Parent studentName="Gagan" />
     </div>
   );
 }
