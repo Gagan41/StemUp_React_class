80a631f9bdd880772294ff3c4cfe6fa32c5ee183
Gagan41 <gagant.p100@gmail.com>
2025-10-17 12:48:23 +0530

    react static froms

---
+++ src/components/htmlForms.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HtmlForms() {
+  return (
+    <div>
+      <form action="#">
+        <input type="email" id="user" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default HtmlForms;
