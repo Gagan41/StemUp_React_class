24afb7a6cf8290e2befaa34fa32697215f070ea7
Gagan41 <gagant.p100@gmail.com>
2025-10-17 12:45:02 +0530

    react css types

---
+++ src/components/css.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const style = {
+  backgroundColor: "lightblue",
+  padding: "10px",
+  borderRadius: "5px",
+};
+//inline css
+// function Greetings(){
+//     return <h2  style={style}>Hello inline styling</h2>
+// }
+
+function Temperature({ value }) {
+  return (
+    <div>
+      <h2 style={style}>Hello inline styling</h2>
+      <p style={{ color: value > 30 ? "red" : "blue" }}>
+        Tempearture :{value} ^C
+      </p>
+    </div>
+  );
+}
+
+export default Temperature;
