0a16bc103d84e5f566b498d3189e655c221d4708
Gagan41 <gagant.p100@gmail.com>
2025-10-17 12:34:37 +0530

    react class component

---
+++ src/components/classOne.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Fruit extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is class component</h1>
+      </div>
+    );
+  }
+}
+
+export default Fruit;
