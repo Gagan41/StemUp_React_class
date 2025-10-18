fb7edb9fe428bbcab401a15ba010d67a23f423fb
Gagan41 <gagant.p100@gmail.com>
2025-10-18 09:22:23 +0530

    17-10-25 class logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
 import "./App.css";
-//import Hello from "./helloworld";
-// import FirstComponent from "./firstComponent";
-// import Fruit from "./classOne";
-// import Parent from "./propsChildren";
-// import ClassResult from "./conditionalRendering";
-// import LogicalAND from "./logicalAND";
-// import Destructuring1 from "./destructuring1";
-// import Destructuring2 from "./destructuring2";
-// import Parent from "./propDrilling";
-import HtmlForms from "./htmlForms";``
-import ControlledForm from "./controlledForm";
-import TwoWayBinding from "./twoWayBinding";
-import Formvalidation from "./formvalidation";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//main class files
+// import Hello from "./components/helloWorld";
+// import FirstComponent from "./components/firstComponent";
+// import Fruit from "./components/classOne";
+// import Parent from "./components/propsChildren";
+// import ClassResult from "./components/conditionalRendering";
+// import LogicalAND from "./components/logicalAND";
+// import Destructuring1 from "./components/destructuring1";
+// import Destructuring2 from "./components/destructuring2";
+// import Parent1 from "./components/propDrilling";
+// import HtmlForms from "./components/htmlForms";
+// import ControlledForm from "./components/controlledForm";
+// import TwoWayBinding from "./components/twoWayBinding";
+// import Formvalidation from "./components/formValidation";
+// import Example from "./components/hooks";
+// import Counter from "./components/counter";
+// import Cleanup from "./components/cleanup";
+// import UserList from "./components/userList";
+// import Temperature from "./components/css";
+// import External from "./components/external";
+// import Button from "./components/button";
+// import ProductCard from "./components/productCard";
+import Index from "./components/index";
+import Home from "./components/home";
+import Contact from "./components/contact";
+import About from "./components/about";
+
+//Assignments
+// import ShopApp from "./Assignments/shopApp";
 
 function App() {
   return (
     <div className="App">
-      {/* <FirstComponent name="Gagan" /> */}
-      {/* <FirstComponent name="Singh" />
-      <FirstComponent name="Yadav" /> */}
-      {/* <Fruit />
-      <Parent /> */}
-      {/* <ClassResult isresult={false}/>
-      <LogicalAND brand="Ford"/>
-      <Destructuring1 color="Red"/>
+      <h1>This is a app file</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+      {/* Class work */}
+      {/* <FirstComponent name="Gagan" />
+      <FirstComponent name="Singh" />
+      <FirstComponent name="Yadav" />
+      <Fruit />
+      <Parent />
+      <ClassResult isresult={false} />
+      <LogicalAND brand="Ford" />
+      <Destructuring1 color="Red" />
       <Destructuring2 brand="Ford" model="Mustang" />
-      <Parent studentName="Gagan" /> */}
+      <Parent1 studentName="Gagan" />
       <HtmlForms />
       <ControlledForm />
       <TwoWayBinding />
       <Formvalidation />
+      <Example />
+      <Counter />
+      <Cleanup />
+      <UserList />
+      <Temperature value={35} /> */}
+      {/* <External />
+      <Button />
+      <ProductCard /> */}
+
+      {/* Assignments */}
+      {/* <ShopApp /> */}
     </div>
   );
 }
